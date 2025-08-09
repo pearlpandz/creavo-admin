c8c3e5a3f323e18543d2bc5457758e8032b565e3
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-08-09 19:42:39 +0530

    event api updates

--- api/serializers/event.py
+++ api/serializers/event.py
@@ -1,7 +1,16 @@
+
 from rest_framework import serializers
 from api.models.event import Event
+from api.models.event import EventMedia
+class EventMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventMedia
+        fields = ['id', 'media', 'image', 'media_type']
 
 class EventSerializer(serializers.ModelSerializer):
+    images = EventMediaSerializer(source='events', many=True, read_only=True)
+
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = ['id', 'name', 'date', 'images']
+
