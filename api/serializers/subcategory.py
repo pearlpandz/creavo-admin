5a77373680fec99a370e2a0163f6246d0c439f16
kiran19990518 <MamidiK@cryptoassetrating.com>
2025-10-13 20:45:28 +0530

    prod db

--- api/serializers/subcategory.py
+++ api/serializers/subcategory.py
@@ -5,18 +5,21 @@ from api.models.media import Media
 
 # This serializer used only for get list of subcategory under category api
 class SubCategorySerializer(serializers.ModelSerializer):
-    media = serializers.SerializerMethodField()
+    # media = serializers.SerializerMethodField()
     media_count = serializers.SerializerMethodField()
     class Meta:
         model = SubCategory
         fields = ['id', 'name', 'description','media_count']
         exclude = ['category']
 
+    def get_media_count(self, obj):
+        return obj.media.count()
+
     def create(self, validated_data):
         media_data = validated_data.pop('media', [])
         subcategory = SubCategory.objects.create(**validated_data)
         for media_item in media_data:
-            Media.objects.create(subcategories=[subcategory], **media_item)
+            Media.objects.create(subcategories=subcategory, **media_item)
         return subcategory
 
     def update(self, instance, validated_data):
@@ -25,5 +28,5 @@ class SubCategorySerializer(serializers.ModelSerializer):
             setattr(instance, attr, value)
         instance.save()
         for media_item in media_data:
-            Media.objects.create(subcategories=[instance], **media_item)
+            Media.objects.create(subcategories=instance, **media_item)
         return instance
