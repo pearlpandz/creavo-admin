d2d6b253ff94794731e6d7375db0f53d56870831
kiran19990518 <MamidiK@cryptoassetrating.com>
2025-10-13 19:35:56 +0530

    prod db

--- myapp/settings.py
+++ myapp/settings.py
@@ -19,7 +19,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
+env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 MEDIA_URL = '/uploads/'
