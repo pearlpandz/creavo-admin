dfcf21c90ee8b9c6ce2347892cc50195b0daefd5
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-12-23 23:59:59 +0530

    react build update

--- react/src/constants/index.js
+++ react/src/constants/index.js
@@ -1,4 +1,4 @@
 export const SETTINGS = {
-  api_endpoint: "https://dev-frame-service.creavo.in",
-  admin_endpoint: "https://dev-admin.creavo.in",
+  api_endpoint: "https://frame-service.creavo.in",
+  admin_endpoint: "https://admin.creavo.in",
 };
